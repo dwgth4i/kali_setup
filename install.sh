fc88d1fcdfed089303c82f6e51ac42a78e6802a0
Trinh Thai Duong <91010847+dwgth4i@users.noreply.github.com>
2024-07-04 13:04:58 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt-get install -y wget curl git thunar
+sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
+sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
+sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
+
+mkdir -p ~/.local/share/fonts/
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Iosevka.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/RobotoMono.zip
+
+unzip Iosevka.zip -d ~/.local/share/fonts/
+unzip RobotoMono.zip -d ~/.local/share/fonts/
+
+fc-cache -fv
+
+wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.10.0-rc4-1/alacritty_0.10.0-rc4-1_amd64_bullseye.deb
+sudo dpkg -i alacritty_0.10.0-rc4-1_amd64_bullseye.deb
+sudo apt install -f
+
+sudo apt install burpsuite
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# add zsh-autosuggestions to .zshrc plugins
+
+curl -sS https://starship.rs/install.sh | sh
+wget https://raw.githubusercontent.com/dwgth4i/kali_setup/main/starship.toml
+mv starship.toml ./.config/starship.toml
+
+# wallpaper at 
+# https://www.yuumeiart.com/#/revive/
+
+
