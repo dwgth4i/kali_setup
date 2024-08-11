4077fd540d44d5efb835eebe0e72e13d570413a2
Trinh Thai Duong <91010847+dwgth4i@users.noreply.github.com>
2024-08-12 02:08:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,18 @@ sudo apt update && sudo apt upgrade -y
 mkdir ~/tools; cd ~/tools; git clone https://github.com/CravateRouge/bloodyAD
 
 sudo apt-get -y update && sudo apt-get -y install gcc clang libclang-dev libgssapi-krb5-2 libkrb5-dev libsasl2-modules-gssapi-mit musl-tools gcc-mingw-w64-x86-64
+
+
+
 cd ~/tools; git clone https://github.com/NH-RED-TEAM/RustHound
-cd RustHound
-make install
 
 mkdir ~/tools/sliver; cd ~/tools/sliver; wget https://github.com/BishopFox/sliver/releases/download/v1.5.42/sliver-server_linux; chmod +X ~/tools/sliver-server_linux
 
 mkdir -p ~/.local/share/fonts/
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Iosevka.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/RobotoMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip
 
-unzip Iosevka.zip -d ~/.local/share/fonts/
-unzip RobotoMono.zip -d ~/.local/share/fonts/
+unzip 0xProto.zip -d ~/.local/share/fonts/
 
 fc-cache -fv
 
@@ -27,7 +26,7 @@ sudo apt install -f
 
 sudo apt install burpsuite
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
 
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
